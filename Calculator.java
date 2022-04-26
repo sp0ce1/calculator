95c0f00c7e898bfee1022859bca90a7a6f3a69c4
Jonibek <jonik0744clash@gmail.com>
2022-04-26 13:48:42 +0900

    first commit

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    public static void main (String[] args) {
+       // 1
+        int mynum = 5+2*2;
+        System.out.println(mynum);
+
+        // 2
+        int x=10, y=15, z=20;
+        System.out.println(x+y+z);
+
+        //3
+        float a= 2.15E01f;
+        int b= 25;
+        int c= 72;
+        System.out.println(a*b-c);
+    }
+}
